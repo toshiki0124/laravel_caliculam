4a7579a2150c66425e9e34a622a7888ef7ffebe8
toshiki_0124 <ts.hiki1212@gamail.com>
2021-02-26 17:01:43 +0900

    [add]ページネーション

--- app/Post.php
+++ app/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
-    public function getPaginateBylimit(int $limit_count = 1)
+    public function getPaginateBylimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
