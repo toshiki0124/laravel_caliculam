e83e8164d2f54ce9d95157bfc5b6fc0fb3a24d64
toshiki_0124 <ts.hiki1212@gamail.com>
2021-02-26 16:56:22 +0900

    [add]ページネーション

--- app/Post.php
+++ app/Post.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-        public function getByLimit(int $limit_count = 10)
+    public function getByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+    public function getPaginateBylimit(int $limit_count = 1)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 
 }
