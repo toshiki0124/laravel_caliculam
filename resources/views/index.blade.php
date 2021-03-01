d4479059f45ffdb66e399d21d9f74c9e60db877b
toshiki_0124 <ts.hiki1212@gamail.com>
2021-03-01 16:22:57 +0900

    [add]詳細画面2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,11 +11,11 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
+                    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                 </div>
             @endforeach
         </div>
+
         <div class='paginate'>
             {{ $posts->links() }} 
         </div>
