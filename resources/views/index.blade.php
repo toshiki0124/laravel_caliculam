424f7e6ccae97662cde062ce07167b1e4eee275b
toshiki_0124 <ts.hiki1212@gamail.com>
2021-03-01 15:31:50 +0900

    [add]詳細画面

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-',app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+                </div>
+            @endforeach
+        </div>
+
+        <div class='paginate'>
+            {{ $posts->links() }} 
+        </div>
+    </body>
+</html>
