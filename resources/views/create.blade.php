d4563db27313c2cbe9456f551e5f1f276686b1a6
toshiki_0124 <ts.hiki1212@gamail.com>
2021-03-05 19:12:08 +0900

    [add]バリデーション

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,11 +8,13 @@
         <form action="/posts" method="POST">
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。" value="{{ old('post.body') }}"></textarea>
+                <p class="title__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="store"/>
         </form>
